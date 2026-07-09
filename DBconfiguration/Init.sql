3a0216244dc239b3fc601ec2e5a5b57f177907f2
sinnek14-png <sin.nek14@gmail.com>
2026-07-09 16:40:30 +0000

    #

---
+++ DBconfiguration/Init.sql
@@ -0,0 +1,17 @@
+-- Crear tabla para usuarios
+CREATE TABLE usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(255) UNIQUE,
+    telefono VARCHAR(15),
+    fecha_nacimiento DATE
+);
+
+-- Crear la tabla para usuarios y contraseñas
+CREATE TABLE credenciales (
+    id_credencial SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
+);
